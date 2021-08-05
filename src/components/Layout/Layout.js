97cb2a8fdbbf545965639129ef8e41ef00f9b87d
Anonav0 <commonpeople27@gmail.com>
2021-08-05 13:26:16 +0530

    resue the Backdrop

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Aux';
 import classes from './Layout.module.css';
@@ -7,14 +7,26 @@ import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const Layout = ( props ) => (
-    <Aux>
-        <Toolbar />
-        <SideDrawer/>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
+class Layout extends Component {
+
+    state = {
+        showSideDrawer : true
+    }
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar />
+                <SideDrawer open = {this.state.showSideDrawer} closed = {this.sideDrawerClosedHandler}/>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>            
+        )
+    }
+}
 
 export default Layout;
