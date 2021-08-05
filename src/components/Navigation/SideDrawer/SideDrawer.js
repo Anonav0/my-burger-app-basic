c3d2ba027077d856a277ec59f1fbaee97078dab0
Anonav0 <commonpeople27@gmail.com>
2021-08-05 12:41:09 +0530

    work on Responsive Adjustments

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -9,7 +9,9 @@ const sideDrawer = (props) => {
 
     return(
         <div className={classes.SideDrawer}>
-            <Logo/>
+            <div className={classes.Logo}>
+                <Logo/>
+            </div>
             <nav>
                 <NavigationItems/>
             </nav>    
