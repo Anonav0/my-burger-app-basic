0f4d6e614d779a2053fe60ef8bb15c5c804cbef0
Anonav0 <commonpeople27@gmail.com>
2021-08-05 12:56:58 +0530

    more about Responsive Adjustments

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -11,7 +11,7 @@ const toolbar = (props) => (
             <Logo /> 
         </div>
         
-        <nav>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems/>
         </nav>
     </header>
