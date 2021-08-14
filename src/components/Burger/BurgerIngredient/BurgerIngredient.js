a629e57758a7fed89d45e199f9d1d373e14c7d60
Anonav0 <commonpeople27@gmail.com>
2021-08-14 14:00:00 +0530

    breadtop fix

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -15,8 +15,8 @@ class BurgerIngredient extends Component {
         case ('bread-top') :
             ingredient = (
             <div className={classes.BreadTop}>
-                <div className={classes.Seed1}></div>
-                <div className={classes.Seed2}></div>
+                <div className={classes.Seeds1}></div>
+                <div className={classes.Seeds2}></div>
             </div>);
             break;
         case ('meat') :
