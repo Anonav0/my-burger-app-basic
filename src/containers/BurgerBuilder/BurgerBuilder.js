87335f391f4af32504c142c272de3f0ef4cfef83
Anonav0 <commonpeople27@gmail.com>
2021-08-31 19:41:21 +0530

    send a POST request

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -7,6 +7,8 @@ import Modal from '../../components/UI/Modal/Modal';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 const INGREDIENT_PRICES = {
     salad : 0.5,
     cheese : 0.4,
@@ -93,7 +95,29 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-        alert("You continue!");
+        // alert("You continue!");
+        const order = {
+            ingredients : this.state.ingredients,
+            price : this.state.totalPrice,
+            customer : {
+                name : 'Swarnavo Khanra',
+                address : {
+                    street : 'TesterRoad',
+                    zipCode : '3124',
+                    country : 'India'
+                },
+                email: 'test@tester.com',
+
+            },
+            deliveryMethod : 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then( response => {
+                console.log(response);
+            })
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render() {
