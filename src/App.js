f03d53027e4a9d5c4655945861e50fa38c20d45d
Anonav0 <commonpeople27@gmail.com>
2021-09-14 11:01:15 +0530

    building the checkout container

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 
 class App extends Component {
@@ -20,6 +21,7 @@ class App extends Component {
           {/* FOR TEST PURPOSE ONLY */}
           {/* {this.state.show ?<BurgerBuilder/> : null} */}
           <BurgerBuilder/>
+          <Checkout/>
         </Layout>
       </div>
     );
