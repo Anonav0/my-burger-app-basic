cb2c9f0ddeab6834db5b2a0ebe860e03a1cdc64b
Anonav0 <commonpeople27@gmail.com>
2021-09-14 11:45:23 +0530

    navigating back & to next page

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -11,10 +11,21 @@ class Checkout extends Component {
             meat : 1
         }
     }
+
+    checkoutCancelled = () => {
+        this.props.history.goBack(); 
+    }
+
+    checkoutContinued =() => {
+       this.props.history.replace('/checkout/contact-data');
+    }
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients}/>
+                <CheckoutSummary 
+                ingredients={this.state.ingredients}
+                checkoutCancelled={this.checkoutCancelled}
+                checkoutContinued={this.checkoutContinued}/>
             </div>
         )
     }
