b4628f06fb2375a0a9ea02badde98260e799d8a6
Anonav0 <commonpeople27@gmail.com>
2021-09-14 11:16:37 +0530

    Setting up Routing & Routes

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
+import {Route, Switch} from 'react-router-dom';
+
 import Layout from './hoc/Layout/Layout'
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 
 
 class App extends Component {
-  //FOR TEST PURPOSE
-  // state = {
-  //   show : true
-  // }
-  // componentDidMount() {
-  //   setTimeout(() =>{
-  //     this.setState({show: false});
-  //   }, 5000 )
-  // }
+
   render() {
     return (
       <div >
         <Layout>
-          {/* FOR TEST PURPOSE ONLY */}
-          {/* {this.state.show ?<BurgerBuilder/> : null} */}
-          <BurgerBuilder/>
-          <Checkout/>
+          <Switch>
+            <Route path="/checkout" component={Checkout}/>
+            <Route path="/" exact component={BurgerBuilder}/>
+          </Switch>
         </Layout>
       </div>
     );
