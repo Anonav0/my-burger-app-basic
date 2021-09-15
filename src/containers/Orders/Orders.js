a6e0a62fb8854d36420b269b1a951769ccd36a31
Anonav0 <commonpeople27@gmail.com>
2021-09-15 12:35:37 +0530

    implementing Naviagation Links

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'React';
+import React, {Component} from 'react';
 
 import Order from '../../components/Order/Order';
 
@@ -6,7 +6,7 @@ class Orders extends Component {
     render() {
         return(
             <div>
-                <Order />
+                <Order/>
                 <Order/>
             </div>
         );
