b217f9966a0447a862ff5648ab78dbe3dced8d1f
Anonav0 <commonpeople27@gmail.com>
2021-10-04 21:06:55 +0530

    Hadling Overall Form Validity

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -9,7 +9,7 @@ import Input from '../../components/UI/Input/Input';
 class ContactData extends Component {
     state = {
         orderForm : {
- 
+
             name : {
                 elementType: 'input',
                 elementConfig: {
@@ -83,9 +83,11 @@ class ContactData extends Component {
                     options : [{value: 'fastest', displayValue: 'Fastest'},
                                {value: 'cheapest', displayValue: 'Cheapest'}]
                 },
-                value: ''
+                value: '',
+                valid : true
             }
         },
+        formIsValid: false,
         loading: false
     }
 
@@ -146,8 +148,13 @@ class ContactData extends Component {
         updatedFormElement.valid = this.checkValidity(updatedFormElement.value,updatedFormElement.validation);
         updatedFormElement.touched = true;
         updatedOrderForm[inputIdentifier] = updatedFormElement;
-        console.log(updatedFormElement);
-        this.setState({orderForm: updatedOrderForm});
+
+        let formIsValid = true;
+        for ( let inputIdentifier in updatedOrderForm) {
+            formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+        }
+
+        this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
     }
     
     render () {
@@ -171,7 +178,7 @@ class ContactData extends Component {
                         touched={formElement.config.touched}
                         changed={(event) => this.inputChangedHandler(event, formElement.id)}/>
                 ))}
-                <Button btnType="Success" >ORDER</Button>
+                <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
             </form>);
         if(this.state.loading) {
             form = <Spinner/>
