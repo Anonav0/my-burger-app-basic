7bbf5a3a747a2d8fa67593be98e8ff7561d950e9
Anonav0 <commonpeople27@gmail.com>
2022-02-16 21:59:03 +0530

    Basic Redux Setup

--- src/index.js
+++ src/index.js
@@ -4,13 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from 'react-router-dom';
+import { Provider } from 'react-redux';
+import {createStore} from 'redux';
+
+import reducer from './store/reducer';
+
+const store = createStore(reducer);
 
 const app = (
-  <React.StrictMode>
+  <Provider store={store}>
     <BrowserRouter>
       <App/>
     </BrowserRouter>
-  </React.StrictMode>
+  </Provider>
+    
+
 )
 
 ReactDOM.render(
