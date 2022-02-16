cfdaf58ff2f8d2815dc816239e4e1a1e34d018d4
Anonav0 <commonpeople27@gmail.com>
2022-02-16 21:55:16 +0530

    Install Redux and React Redux

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 
 import Button from '../../components/UI/Button/Button';
-import classes from './ContactData.module.css'
+import classes from './ContactData.module.css';
 import axios from '../../../src/axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import Input from '../../components/UI/Input/Input';
---
+++ src/store/reducer.js
@@ -0,0 +1,12 @@
+import * as actionTypes from './action';
+
+const initialState = {
+    ingredients : null,
+    totalPrice: 4
+};
+
+const reducer = (state = initialState, action) => {
+
+};
+
+export default reducer;
