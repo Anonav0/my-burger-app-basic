26a3f22aff261660894fc17716a533990a9c987f
Anonav0 <commonpeople27@gmail.com>
2022-02-17 18:56:11 +0530

    Adjusting Checkout and Contact Data

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -67,22 +67,7 @@ class BurgerBuilder extends Component {
     };
 
     purchaseContinueHandler = () => {
-
-        // alert("You continue!");
-
-        //sending the ingredients information through queryParams to the checkout page.
-        const queryParams = [];
-        
-        for ( let i in this.state.ingredients) {
-            queryParams.push(encodeURIComponent(i)+ '=' + encodeURIComponent(this.state.ingredients[i]));
-
-        }
-        queryParams.push('price=' + this.state.totalPrice);
-        const queryString = queryParams.join('&')
-        this.props.history.push({
-            pathname: '/checkout',
-            search: '?' + queryString
-        });
+        this.props.history.push('/checkout');
 
     }
 
--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux'
 
 import Button from '../../components/UI/Button/Button';
 import classes from './ContactData.module.css';
@@ -101,7 +102,7 @@ class ContactData extends Component {
             formData[formElementIdentifier] = this.state.orderForm[formElementIdentifier].value
         }
         const order = {
-            ingredients : this.props.ingredients,
+            ingredients : this.props.ings,
             price : this.props.price,
             orderData : formData
         }
@@ -193,4 +194,11 @@ class ContactData extends Component {
     }
 } 
 
-export default ContactData;
+const mapStateToProps = state => {
+    return {
+        ings : state.ingredients,
+        price: state.totalPrice
+    }
+}
+
+export default connect(mapStateToProps)(ContactData);
