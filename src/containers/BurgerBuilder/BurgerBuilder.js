afb9da277dedc5edd25d56bed98521b38ac6b35e
Anonav0 <commonpeople27@gmail.com>
2022-02-17 18:42:24 +0530

    Redux & UI State

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -25,10 +25,6 @@ class BurgerBuilder extends Component {
     //     this.state = {...}
     // }
     state = {
-
-        
-        
-        purchaseable: false,
         purchasing: false,
         loading: false,
         error : false
@@ -56,45 +52,11 @@ class BurgerBuilder extends Component {
                 return sum+el;
             },0);
 
-        this.setState ({purchaseable: sum > 0});
+        return  sum > 0;
         
     }
 
-    // addIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     const updatedCount = oldCount + 1;
-    //     const updatedIngredient = {
-    //         ...this.state.ingredients
-    //     };
-    //     updatedIngredient[type] = updatedCount;
 
-    //     const priceAddition = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.state.totalPrice;
-    //     const newPrice = oldPrice + priceAddition;
-    //     this.setState({totalPrice: newPrice, ingredients: updatedIngredient});
-    //     this.updatePurchaseState(updatedIngredient);
-
-
-    // };
-
-    // removeIngredientHandler = (type) => {
-    //     const oldCount = this.state.ingredients[type];
-    //     if (oldCount <= 0) {
-    //         return;
-    //     }
-    //     const updatedCount = oldCount - 1;
-    //     const updatedIngredient = {
-    //         ...this.state.ingredients
-    //     };
-    //     updatedIngredient[type] = updatedCount;
-
-    //     const priceDeduction = INGREDIENT_PRICES[type];
-    //     const oldPrice = this.state.totalPrice;
-    //     const newPrice = oldPrice - priceDeduction;
-    //     this.setState({totalPrice: newPrice, ingredients: updatedIngredient});
-    //     this.updatePurchaseState(updatedIngredient);
-
-    // };
 
     purchasingHandler =()=> {
         this.setState({purchasing: true});
@@ -145,7 +107,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded= {this.props.onIngredientAdded}
                         ingredientRemoved = {this.props.onIngredientRemoved}
                         disabled = {disabledInfo}
-                        purchaseable = {this.state.purchaseable}
+                        purchaseable = {this.updatePurchaseState(this.props.ings)}
                         price = {this.props.price}
                         ordered = {this.purchasingHandler}
                          />
