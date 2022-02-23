82065ecec639ec106e9e7ee727ae03acaf197cd8
Anonav0 <commonpeople27@gmail.com>
2022-02-23 21:48:45 +0530

    Work on Order Actions

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -91,8 +91,7 @@ class ContactData extends Component {
                 valid : true
             }
         },
-        formIsValid: false,
-        loading: false
+        formIsValid: false
     }
 
     orderHandler = (event) => {
@@ -176,7 +175,7 @@ class ContactData extends Component {
                 ))}
                 <Button btnType="Success" disabled={!this.state.formIsValid} >ORDER</Button>
             </form>);
-        if(this.state.loading) {
+        if(this.props.loading) {
             form = <Spinner/>
         }
         return (
@@ -191,14 +190,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings : state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
-export default connect(mapStateToProps)(withErrorHandler(ContactData, axios));
+export default connect(mapStateToProps, mapDispatchToProps)(withErrorHandler(ContactData, axios));
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -7,6 +7,12 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
+        case  actionTypes.PURCHASE_BURGER_START :
+            return {
+                ...state,
+                loading: true
+            }
+        
         case actionTypes.PURCHASE_BURGER_SUCCESS:
             const newOrder  = {
                 ...action.orderData,
