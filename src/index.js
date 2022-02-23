c491dcd727d83bdf40ef9f382437a19156829e66
Anonav0 <commonpeople27@gmail.com>
2022-02-23 19:37:15 +0530

    Prepare the Folder Structure

--- src/index.js
+++ src/index.js
@@ -9,7 +9,10 @@ import {createStore} from 'redux';
 
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
+const store = createStore(reducer,
+   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+
+  );
 
 const app = (
   <Provider store={store}>
