ee9bd30218bdb2b155403f61a84754d5de02f7aa
Anonav0 <commonpeople27@gmail.com>
2022-02-23 22:04:08 +0530

    Combining Reducers

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -86,7 +86,7 @@ class ContactData extends Component {
                     options : [{value: 'fastest', displayValue: 'Fastest'},
                                {value: 'cheapest', displayValue: 'Cheapest'}]
                 },
-                value: '',
+                value: 'fastest',
                 validation: {},
                 valid : true
             }
@@ -189,9 +189,9 @@ class ContactData extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings : state.ingredients,
-        price: state.totalPrice,
-        loading: state.loading
+        ings : state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        loading: state.order.loading
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,13 +5,18 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter} from 'react-router-dom';
 import { Provider } from 'react-redux';
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux';
 import thunk from 'redux-thunk';
 
 import burgerBuilderReducer from './store/reducers/burgerBuilder';
+import orderReducer from './store/reducers/order';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(burgerBuilderReducer, composeEnhancers(
+const rootReducer = combineReducers( {
+  burgerBuilder: burgerBuilderReducer,
+  order: orderReducer
+})
+const store = createStore(rootReducer, composeEnhancers(
       applyMiddleware(thunk)
     )
 
