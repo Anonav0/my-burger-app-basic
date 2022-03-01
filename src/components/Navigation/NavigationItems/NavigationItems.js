1ddd1991e1f7182aac3f7d9a1914d7c63f6f1af5
Anonav0 <commonpeople27@gmail.com>
2022-03-01 20:30:41 +0530

    Add an auth Form

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -7,6 +7,7 @@ const navigationItems = (props) => (
     <ul className={classes.NavigationItems}>
         <NavigationItem link='/' exact>BurgerBuilder</NavigationItem>
         <NavigationItem link='/orders'>Orders</NavigationItem>
+        <NavigationItem link='/auth'>Authenticate</NavigationItem>
     </ul>
 );
 
