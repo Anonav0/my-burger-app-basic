b4bf5b3ccc07edb9ad827a2b6bc2a510a61b1a9e
Anonav0 <commonpeople27@gmail.com>
2022-03-02 19:44:33 +0530

    Log Users Out

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -23,6 +23,20 @@ export const authFail = (error) => {
     };
 };
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+};
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000);
+    };
+};
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart());
@@ -38,7 +52,8 @@ export const auth = (email, password, isSignup) => {
         axios.post(url, authData)
             .then(response => {
                 console.log(response);
-                dispatch(authSuccess(response.data.idToken, response.data.localId))
+                dispatch(authSuccess(response.data.idToken, response.data.localId));
+                dispatch(checkAuthTimeout(response.data.expiresIn))
             })
             .catch(err => {
                 console.log(err);
