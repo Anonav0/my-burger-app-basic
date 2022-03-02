9104d8b2da26a00125fcec06edb074b267c54c36
Anonav0 <commonpeople27@gmail.com>
2022-03-02 20:31:13 +0530

    Access Protected Resources

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -13,7 +13,7 @@ class Orders extends Component {
 
 
     componentDidMount() {
-        this.props.onFetchOrders();
+        this.props.onFetchOrders(this.props.token);
     }
     render() {
         let orders = <Spinner/>
@@ -38,13 +38,14 @@ class Orders extends Component {
 const mapStateToProps = state => {
     return {
         orders: state.order.orders,
-        loading: state.order.loading
+        loading: state.order.loading,
+        token : state.auth.token
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onFetchOrders : () => dispatch(actions.fetchOrders())
+        onFetchOrders : (token) => dispatch(actions.fetchOrders(token))
     }
 }
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -23,10 +23,10 @@ export const purchaseBurgerStart = () => {
     }
 }
 
-export const purchaseBurger = (orderData) => {
+export const purchaseBurger = (orderData, token) => {
     return dispatch => {
         dispatch(purchaseBurgerStart());
-        axios.post('/orders.json', orderData)
+        axios.post('/orders.json?auth='+ token, orderData)
             .then( response => {
                 dispatch(purchaseBurgerSuccess(response.data.name, orderData));
             })
@@ -63,9 +63,9 @@ export const fetchOrdersStart = () => {
     };
 };
 
-export const fetchOrders = () => {
+export const fetchOrders = (token) => {
     return dispatch => {
-        axios.get('orders.json')
+        axios.get('orders.json?auth='+ token )
             .then(res => {
                 dispatch(fetchOrdersStart());
                 console.log(res.data)
