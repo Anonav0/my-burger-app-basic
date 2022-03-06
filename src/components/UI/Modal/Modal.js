146afecd8ec5285c881e65179883ee118712e4a7
Anonav0 <commonpeople27@gmail.com>
2022-03-06 18:31:41 +0530

    Add Lazy Loading

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,9 +9,7 @@ class Modal  extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return (nextProps.show !== this.props.show) || (nextProps.children !== this.props.children);
     }
-    componentDidUpdate() {
-        console.log('[Modal.js] DidUpdate');
-    }
+
     render() {
         return (
             <Aux>
