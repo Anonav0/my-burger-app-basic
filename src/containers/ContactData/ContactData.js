5df96cf92dbd0ac8b9a20b8c875d59cccf0ae6fd
Anonav0 <commonpeople27@gmail.com>
2022-03-06 17:50:42 +0530

    Use updateObject in the Entire App

--- src/containers/ContactData/ContactData.js
+++ src/containers/ContactData/ContactData.js
@@ -9,6 +9,8 @@ import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 import axios from '../../axios-orders';
 import * as actions from '../../store/actions/index';
 
+import {updateObject} from '../../shared/utility';
+
 class ContactData extends Component {
     state = {
         orderForm : {
@@ -133,17 +135,16 @@ class ContactData extends Component {
 
     inputChangedHandler = (event, inputIdentifier) => {
         // console.log(event.target.value);
-        const updatedOrderForm = {
-            ...this.state.orderForm
-        }
-        const updatedFormElement = {
-            ...updatedOrderForm[inputIdentifier]
-        }
+        
+        const updatedFormElement = updateObject(this.state.orderForm[inputIdentifier], {
+            value: event.target.value,
+            valid: this.checkValidity(event.target.value,this.state.orderForm[inputIdentifier].validation),
+            touched: true
+        });
+        const updatedOrderForm = updateObject(this.state.orderForm, {
+            [inputIdentifier] : updatedFormElement
+        })
 
-        updatedFormElement.value = event.target.value;
-        updatedFormElement.valid = this.checkValidity(updatedFormElement.value,updatedFormElement.validation);
-        updatedFormElement.touched = true;
-        updatedOrderForm[inputIdentifier] = updatedFormElement;
 
         let formIsValid = true;
         for ( let inputIdentifier in updatedOrderForm) {
