c6d26815c132969ce89ba54e9fdf08a8d6793708
Anonav0 <commonpeople27@gmail.com>
2022-03-14 20:51:12 +0530

    Test Components Continued

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -1,15 +1,23 @@
-import React from 'react';
+import React from "react";
 
-import {configure, shallow} from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
-import NavigationItems from './NavigationItems';
-import NavigationItem from './NavigationItem/NavigationItem';
+import { configure, shallow } from "enzyme";
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+import NavigationItems from "./NavigationItems";
+import NavigationItem from "./NavigationItem/NavigationItem";
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
-describe('<NavigationItems/>', () => {
-    it('should render two <NavigationItem /> elements if not authenticated', () => {
-    const wrapper = shallow(<NavigationItems />);
+describe("<NavigationItems/>", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<NavigationItems />);
+  });
+  it("should render two <NavigationItem /> elements if not authenticated", () => {
     expect(wrapper.find(NavigationItem)).toHaveLength(2);
-    });
+  });
+
+  it("should render three <NavigationItem /> elements if  authenticated", () => {
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.find(NavigationItem)).toHaveLength(3);
+  });
 });
