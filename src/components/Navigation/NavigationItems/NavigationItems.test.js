eae08c2e6450bdba3b7f2953839cff6d0553c529
Anonav0 <commonpeople27@gmail.com>
2022-03-14 21:05:00 +0530

    Jest and Enzyme Documentations

--- src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -20,4 +20,9 @@ describe("<NavigationItems/>", () => {
     wrapper.setProps({isAuthenticated: true})
     expect(wrapper.find(NavigationItem)).toHaveLength(3);
   });
+
+  it("should render three <NavigationItem /> elements if  authenticated and show logout", () => {
+    wrapper.setProps({isAuthenticated: true})
+    expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true);
+  });
 });
