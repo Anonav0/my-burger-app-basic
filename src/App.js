b005edacaf7fd579be3b1c7407beb5ab3d3421f0
Anonav0 <commonpeople27@gmail.com>
2022-04-11 18:36:56 +0530

    Route with React.lazy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React, { useEffect } from "react";
+import React, { useEffect, Suspense } from "react";
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
-import asyncComponent from "./hoc/asyncComponent/asyncComponent";
 
 import Layout from "./hoc/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder";
@@ -9,15 +8,15 @@ import Logout from "./containers/Auth/Logout/Logout";
 
 import * as actions from "./store/actions/index";
 
-const asyncCheckout = asyncComponent(() => {
+const Checkout = React.lazy(() => {
   return import("./containers/Checkout/Checkout");
 });
 
-const asyncOrders = asyncComponent(() => {
+const Orders = React.lazy(() => {
   return import("./containers/Orders/Orders");
 });
 
-const asyncAuth = asyncComponent(() => {
+const Auth = React.lazy(() => {
   return import("./containers/Auth/Auth");
 });
 
@@ -28,7 +27,7 @@ const App = (props) => {
 
   let routes = (
     <Switch>
-      <Route path="/auth" component={asyncAuth} />
+      <Route path="/auth" render={() => < Auth/>} />
       <Route path="/" exact component={BurgerBuilder} />
       <Redirect to="/" />
     </Switch>
@@ -37,10 +36,10 @@ const App = (props) => {
   if (props.isAuthenticated) {
     routes = (
       <Switch>
-        <Route path="/checkout" component={asyncCheckout} />
-        <Route path="/orders" component={asyncOrders} />
+        <Route path="/checkout" render={() => <Checkout/>} />
+        <Route path="/orders" render={() => <Orders/>} />
         <Route path="/logout" component={Logout} />
-        <Route path="/auth" component={asyncAuth} />
+        <Route path="/auth" render={() => <Auth/>} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -49,7 +48,7 @@ const App = (props) => {
 
   return (
     <div>
-      <Layout>{routes}</Layout>
+      <Layout><Suspense fallback={<p>Loading...</p>}>{routes}</Suspense></Layout>
     </div>
   );
 };
